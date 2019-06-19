c8a1c6984ca7062f7082a7bd8b973bb1507bdd59
212725511 <212725511@ge.com>
2019-06-19 11:41:07 +0530

    changes

--- dist/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("./index");
-const port = process.env.port ? process.env.port : '8081';
-index_1.default.listen(port, function () {
-    console.log("Server started at port", port);
-});
